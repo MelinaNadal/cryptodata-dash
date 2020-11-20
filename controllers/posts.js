411f58e369de4fcd560d2ca038370b73d2a87893
Melina Nadal <melinanhopkins@gmail.com>
2020-11-19 23:00:43 -0500

    updates proper create route

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,12 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 
-const $name = $('#name');
-const $symbol = $('#symbol');
-const $price = $('#Price');
-const $description = $('#descRiption');
-const $iconURL = $('#iconURL')
-
 const postSeed = require('../models/postSeed');
 router.get('/seed', (req, res) => {
 
@@ -21,6 +15,7 @@ router.get('/seed', (req, res) => {
 
 router.get('/', (req, res) => {
  Post.find({}, (error, allPosts) => {
+
     res.render('dash', {
       posts: allPosts
     })
@@ -35,6 +30,9 @@ router.get('/new', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   Post.findByIdAndDelete(req.params.id, (error, deletedPost) => {
+    
+   
+    
     res.redirect('/dash')
   })
 })
@@ -72,44 +70,3 @@ router.get('/:id', (req, res) => {
 
 
 module.exports = router;
-$('form').on('submit', handleGetData);
-
-function handleGetData(event) {
-	event.preventDefault();
-	$userInput= $('#input-box');
-  userInput = $userInput.val();
-  console.log(userInput)
-  
-
-  
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": `https://coinranking1.p.rapidapi.com/coin/${userInput}`,
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-      "x-rapidapi-key": "c0f227240amsh55aa92563c9e45cp1e4b12jsn976987091817"
-    }
-  }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-    cryptoData.name=response.data.coin.name
-    cryptoData.symbol=response.data.coin.symbol
-    cryptoData.price=response.data.coin.price
-    cryptoData.description=response.data.coin.description
-    cryptoData.iconURL=response.data.coin.iconUrl
-    render()
-  });
-}
-
-
- function render() {
-  $name.text(cryptoData.name);
-  $symbol.text(cryptoData.symbol);
-	$price.text(cryptoData.price);
-  $description.text(cryptoData.description);
-  $iconURL.attr('src', `${cryptoData.iconURL}`);
- }
-  
--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expresstagra
 
 const db = mongoose.connection;
 
+
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:true });
 
 // mongodb error / success ===============
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'))
